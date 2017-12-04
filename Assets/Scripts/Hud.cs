8d6257518c031a4e97b6a4f96f56abb71dccca61
Carles Vallve <carles-vallve@dmm.com>
2017-12-04 19:49:42 +0900

    Implemented unity chan with light and shadow

---
+++ Assets/Scripts/Hud.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hud : MonoBehaviour {
+
+  public Transform model;
+  private float rotIncrement = 0;
+  private float rotSpeed = 3;
+
+  
+  public void SetRotate(float value) {
+    rotIncrement = value * rotSpeed;
+  }
+  
+
+  void Update() {
+    model.Rotate(0, rotIncrement, 0);
+  }
+  
+}
